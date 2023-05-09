48436687606765d65e1e4646c686c0a25070d89f
qxan <mehmet.enfa@gmail.com>
2023-05-09 19:27:57 +0300

    appStyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import React from 'react';
+import './styles/Global.css'
+import 'rsuite/dist/styles/rsuite-default.css'
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
-      asdafd
+      <div id='content'>
+        <Intro></Intro>
+      </div>
     </div>
   );
 }
