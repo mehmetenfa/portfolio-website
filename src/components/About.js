f29703e2e51b7a0bc30d9d7d309328418985577e
qxan <mehmet.enfa@gmail.com>
2023-05-13 23:01:59 +0300

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,86 @@
-import React from 'react'
+import React from "react";
+import "../styles/About.css";
+import FadeInSection from "./FadeInSection";
 
 class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
   render() {
+    const one = (
+      <p>
+        I am currently a <b>Software Development Engineer</b> in Turkey.
+      </p>
+    );
+    const two = (
+      <p>
+        I am interested in following scientific and technological developments
+        outside of work, I constantly read articles and try to follow
+        innovations in software. I also love to play basketball, I play it all
+        the time.
+      </p>
+    );
+    const tree = (
+      <p>
+        <b>Want to chat?</b> Shoot me a message at{" "}
+        <a href="mehmet.enfa@gmail.com">mehmet.enfa@gmail.com</a> and let's
+        talk.
+      </p>
+    );
+    const desc_items = [one, two];
+
+    const tech_stack = [
+      "Javascript ES6+",
+      "Python",
+      "React.js",
+      "React Native",
+      "Next.js",
+      "TypeScript",
+      "HTML & CSS",
+      "Tailwin CSS",
+    ];
+
+    const tech_items = tech_stack.map((stack) => <li>{stack}</li>);
+
     return (
-      <div>About</div>
-    )
+      <div id="about">
+        <FadeInSection>
+          <div className="section-header ">
+            <span className="section-title">/ about me</span>
+          </div>
+          <div className="about-content">
+            <div className="about-description">
+              {[one]}
+              {"Here are some technologies I have been working with:"}
+              <ul className="tech-stack">
+                {tech_stack.map(function (tech_item, i) {
+                  return (
+                    <FadeInSection delay={`${i + 1}00ms`}>
+                      <li>{tech_item}</li>
+                    </FadeInSection>
+                  );
+                })}
+              </ul>
+              {[two]}
+            </div>
+            <div className="about-image">
+              <img src={"/assets/me2.jpg"} />
+            </div>
+          </div>
+        </FadeInSection>
+      </div>
+    );
   }
 }
 
-export default About
+export default About;
