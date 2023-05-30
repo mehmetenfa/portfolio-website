361dd19d53115a5d7bf90ded17caa0e6ecf9a6fa
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-05-30 23:03:15 +0300

    projectsFile

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,143 @@
-import React, { Component } from 'react'
+import React from "react";
+import "../styles/Projects.css";
+import FolderOpenRoundedIcon from "@material-ui/icons/FolderOpenRounded";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import OpenInBrowserIcon from "@material-ui/icons/OpenInBrowser";
+import FadeInSection from "./FadeInSection";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import Carousel from "react-bootstrap/Carousel";
+import ExternalLinks from "./ExternalLinks";
 
-export class Projects extends Component {
+class Projects extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
   render() {
+    const spotlightProjects = {
+      none: {
+        title: "none",
+        desc: "",
+        techStack: "",
+        link: "",
+        open: "",
+        image: "",
+      },
+      none: {
+        title: "none",
+        desc: "",
+        techStack: "",
+        link: "",
+        open: "",
+        image: "",
+      },
+    };
+    const projects = {
+      none: {
+        desc: "",
+        techStack: "",
+        link: "",
+        open: "",
+      },
+      none: {
+        desc: "",
+        techStack: "",
+        link: "",
+        open: "",
+      },
+      none: {
+        desc: "",
+        techStack: "",
+        link: "",
+      },
+      none: {
+        desc: "",
+        techStack: "",
+        link: "",
+        open: "",
+      },
+      none: {
+        desc: "",
+        techStack: "",
+        link: "",
+        open: "",
+      },
+      none: {
+        desc: "",
+        techStack: "",
+        link: "",
+        open: "",
+      },
+    };
+
     return (
-      <div>Projects</div>
-    )
+      <div id="projects">
+        <div className="section-header ">
+          <span className="section-title">/ software-creations</span>
+        </div>
+        <Carousel>
+          {Object.keys(spotlightProjects).map((key, i) => (
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={spotlightProjects[key]["image"]}
+                alt={key}
+              />
+              <div className="caption-bg">
+                <Carousel.Caption>
+                  <h3>{spotlightProjects[key]["title"]}</h3>
+                  <p>
+                    {spotlightProjects[key]["desc"]}
+                    <p className="techStack">
+                      {spotlightProjects[key]["techStack"]}
+                    </p>
+                  </p>
+                  <ExternalLinks
+                    githubLink={spotlightProjects[key]["link"]}
+                    openLink={spotlightProjects[key]["open"]}
+                  ></ExternalLinks>
+                </Carousel.Caption>
+              </div>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+        <div className="project-container">
+          <ul className="projects-grid">
+            {Object.keys(projects).map((key, i) => (
+              <FadeInSection delay={`${i + 1}00ms`}>
+                <li className="projects-card">
+                  <div className="card-header">
+                    <div className="folder-icon">
+                      <FolderOpenRoundedIcon
+                        style={{ fontSize: 35 }}
+                      ></FolderOpenRoundedIcon>
+                    </div>
+                    <ExternalLinks
+                      githubLink={projects[key]["link"]}
+                      openLink={projects[key]["open"]}
+                    ></ExternalLinks>
+                  </div>
+
+                  <div className="card-title">{key}</div>
+                  <div className="card-desc">{projects[key]["desc"]}</div>
+                  <div className="card-tech">{projects[key]["techStack"]}</div>
+                </li>
+              </FadeInSection>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Projects
+export default Projects;
