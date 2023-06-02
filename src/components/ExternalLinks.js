6b9aaa351966a765a492a504872c6157fe29c980
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-06-02 23:47:47 +0300

    externalLinks

--- src/components/ExternalLinks.js
+++ src/components/ExternalLinks.js
@@ -1,11 +1,45 @@
-import React, { Component } from 'react'
+import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import OpenInBrowserIcon from "@material-ui/icons/OpenInBrowser";
 
-export class ExternalLinks extends Component {
+class ExternalLinks extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1"
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey
+    });
+  }
   render() {
     return (
-      <div>ExternalLinks</div>
-    )
+      <span className="external-links">
+        <a className="github-icon" href={this.props.githubLink}>
+          <GitHubIcon
+            style={{
+              fontSize: 20,
+              color: "var(--lightest-slate)"
+            }}
+          ></GitHubIcon>
+        </a>
+        {this.props.openLink && (
+          <a className="open-icon" href={this.props.openLink}>
+            <OpenInBrowserIcon
+              style={{
+                fontSize: 25,
+                color: "var(--lightest-slate)"
+              }}
+            ></OpenInBrowserIcon>
+          </a>
+        )}
+      </span>
+    );
   }
 }
 
-export default ExternalLinks
+export default ExternalLinks;
